6842e3e8272bbbe95aad71a330170e3bbdfc77c1
kukab-shakil <49521206+kukab-shakil@users.noreply.github.com>
2019-06-22 17:39:18 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
 function myfun(){
     let a = document.getElementById("mobilenumber").value;
+    // var cnic_no = '125-1234567-9'
+    // cnic_no_regex = /^[0-9+]{5}-[0-9+]{7}-[0-9]{1}$/;
 
     if(a == ''){
         document.getElementById('message').innerHTML='please fill this column';
       return false;
     }
-    else{
-        document.getElementById('message').innerHTML='Thank you';
-        return true;
+    else if(isNaN(a)){
+        document.getElementById('message').innerHTML='please enter a number';
+        return false;
+        
     }
+
+    else if(a.length<10){
+        document.getElementById('message').innerHTML='mobile no. must be 10 digit';
+        return false;
+
+    }
+    else if(a.length>10){
+        document.getElementById('message').innerHTML='mobile no. must be 10 digit';
+        return false;
+
+    }
+    else if((a.charAt(0)!=9) && (a.charAt(0)!=4 ) && (a.charAt(0)!=3 )){
+        document.getElementById('message').innerHTML='should start with 9 4 3';
+        return false;
+
+    }  if((a.charAt(0) ==9) && (a.charAt(9)==3)){
+        document.getElementById('message').innerHTML="pakistan";
+     document.getElementById('me').innerHTML='karachi';
+
+        
+    }
+//        if(a.charAt(9)==3){
+//      document.getElementById('message').innerHTML='karachi';
+//  }
+//  (variable1 + ", " + variable2);
+  
  }
