66988f75f0a57da56b8abd934fb564d90176a1c4
kukab-shakil <49521206+kukab-shakil@users.noreply.github.com>
2019-06-24 20:35:11 +0500

    Add files via upload

--- scrip.js
+++ scrip.js
@@ -10,20 +10,57 @@ function myfun() {
         document.getElementById('message').innerHTML = 'please fill this column';
         return false;
     }
-   if(a.charAt(0) ==9){
-        document.getElementById('message').innerHTML="karachi";
+    // province
+   if(a.charAt(0) ==1){
+        document.getElementById('province').innerHTML="khyber Pakhton kha";
 
 
     }
+    if(a.charAt(0) ==2){
+        document.getElementById('province').innerHTML=" PROVINCE: FATA";
+
+
+    }if(a.charAt(0) ==3){
+        document.getElementById('province').innerHTML="PROVINCE: punjab";
+
+
+    }if(a.charAt(0) ==4){
+        document.getElementById('province').innerHTML="PROVINCE: Karachi";
+
+
+    }if(a.charAt(0) ==5){
+        document.getElementById('province').innerHTML="PROVINCE: Balochistan";
+
+
+    }if(a.charAt(0) ==6){
+        document.getElementById('province').innerHTML="PROVINCE: Islamabad";
+
+
+    }if(a.charAt(0) ==7){
+        document.getElementById('province').innerHTML="PROVINCE: Giggit Bakdistan";
+
+
+    }
+    // province
+    // if 123456789 in CNIC show error
+    if ((a.charAt(0) != 1) && (a.charAt(0) != 2) && (a.charAt(0) != 3) && (a.charAt(0) != 4) && (a.charAt(0) != 5) && (a.charAt(0) != 6) && (a.charAt(0) != 7)) {
+        document.getElementById('me').innerHTML = 'invali! Pakistan has only SEVEN Province';
+
+        return false;
+
+    }
+
+    // if 123456789 in CNIC show error
+
 
    if ((a.charAt(14) == 1) || (a.charAt(14) == 3) || (a.charAt(14) == 5) || (a.charAt(14) == 7) || (a.charAt(14) == 9)) {
-        document.getElementById('me').innerHTML = ' Hi!boy';
+        document.getElementById('boy').innerHTML = ' Gender: Male';
 
         return false;
 
     }
      if ((a.charAt(14) == 2) || (a.charAt(14) == 4) || (a.charAt(14) == 6) || (a.charAt(14) == 8) || (a.charAt(14) == 0)) {
-        document.getElementById('me').innerHTML = ' Hi! girl';
+        document.getElementById('boy').innerHTML = ' Gender: Female';
 
         return false;
 
